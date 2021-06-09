2d2b6e75a9681fca9ffb76dde9ceb4d8326f4a35
wasd003 <chenhuiji2000@gmail.com>
2021-06-09 18:36:19 +0800

    修改接口返回格式

--- MIE/Controllers/AuthController.cs
+++ MIE/Controllers/AuthController.cs
@@ -9,44 +9,51 @@ using MIE.Entity;
 using MIE.Utils;
 
 
-namespace MIE.Controllers {
-  [Route("/auth")]
-  public class AuthController : Controller {
-    private readonly IUserDao userDao;
-    private readonly IAuthUtil authUtil;
+namespace MIE.Controllers
+{
+    [Route("/auth")]
+    public class AuthController : Controller
+    {
+        private readonly IUserDao userDao;
+        private readonly IAuthUtil authUtil;
 
-    public AuthController(IUserDao userDao, IAuthUtil authUtil) {
-      this.userDao = userDao;
-      this.authUtil = authUtil;
+        public AuthController(IUserDao userDao, IAuthUtil authUtil)
+        {
+            this.userDao = userDao;
+            this.authUtil = authUtil;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] User loginUser)
+        {
+            string username = loginUser.Username;
+            string password = loginUser.Password;
+            User tar = userDao.GetUserByUsername(username);
+            if (tar != null && tar.Password == password)
+                return Ok(ResponseUtil.SuccessfulResponse("成功登陆",
+                    new { token = authUtil.GetToken(tar) }));
+            else
+                return Ok(ResponseUtil.ErrorResponse(10000, "用户名或密码错误"));
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] User user)
+        {
+            if (user.Password == null || user.Password.Length < 6 || user.Username == null)
+                return Ok(ResponseUtil.ErrorResponse(10001, "密码太短或者用户名为空"));
+            if (userDao.GetUserByUsername(user.Username) != null)
+                return Ok(ResponseUtil.ErrorResponse(10002, "用户已存在"));
+
+            bool addRes = userDao.AddUser(user);
+            return addRes ? Ok(ResponseUtil.SuccessfulResponse("注册成功")) :
+                      Ok(ResponseUtil.ErrorResponse(10003, "注册失败"));
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult TestToken()
+        {
+            return Ok(new { id = authUtil.GetIdFromToken() });
+        }
     }
-
-    [HttpPost("login")]
-    public IActionResult Login([FromBody] User loginUser) {
-      string username = loginUser.Username;
-      string password = loginUser.Password;
-      User tar = userDao.GetUserByUsername(username);
-      if (tar != null && tar.Password == password) {
-        return Ok(new { token = authUtil.GetToken(tar) });
-      } else {
-        return Ok(new { msg = "未找到该用户"});
-      }
-    }
-
-    [HttpPost("register")]
-    public IActionResult Register([FromBody] User user) {
-      if (user.Password == null || user.Password.Length < 6 || user.Username == null)
-        return Ok(new { msg = "密码太短或者用户名为空"});
-      if (userDao.GetUserByUsername(user.Username) != null)
-        return Ok(new { msg = "用户名重复"});
-
-      bool addRes = userDao.AddUser(user);
-      return addRes ? Ok(new { msg = "注册成功" }) : Ok(new { msg = "注册失败" });
-    }
-
-    [Authorize]
-    [HttpGet]
-    public IActionResult TestToken() {
-      return Ok(new { id = authUtil.GetIdFromToken() });
-    }
-  }
 }
---
+++ MIE/Utils/ResponseUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace MIE.Utils
+{
+    public class ResponseUtil
+    {
+        public int Code { get; set; }
+        public string Msg { get; set; }
+        public object Data { get; set; }
+
+        public static ResponseUtil SuccessfulResponse(string msg,
+            object data = null) => new ResponseUtil(0, msg, data);
+
+        public static ResponseUtil ErrorResponse(int code,
+            string msg, object data = null) => new ResponseUtil(code, msg, data);
+
+        public ResponseUtil(int code, string msg, object data)
+        {
+            this.Code = code;
+            this.Msg = msg;
+            this.Data = data;
+        }
+    }
+}
